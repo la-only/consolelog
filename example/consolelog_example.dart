8bdde350e9a4c63220fda64b93c4b3510f95d1ea
LA <i@0ls.ru>
2021-12-07 13:15:46 +0700

    init

---
+++ example/consolelog_example.dart
@@ -0,0 +1,6 @@
+import 'package:consolelog/consolelog.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/consolelog.dart
@@ -0,0 +1,23 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library consolelog;
+
+export 'src/consolelog_base.dart';
+
+import 'dart:developer' as developer;
+
+// ignore: camel_case_types
+class console
+{
+  static void log(var everyThing)
+  {
+    try {
+      // ignore: unnecessary_brace_in_string_interps
+      developer.log('${everyThing}', name: '🔵console.log\t\t\t') ;
+    } catch (e) {
+      // ignore: unnecessary_brace_in_string_interps
+      developer.log('${e}', name: '🔴console.log TRY ERROR\t\t\t') ;
+    }
+  }
+}
